37e687564cbd51ec7e91909a834a1608d3f32e74
Fernando <fergkz@gmail.com>
2025-02-11 20:44:38 -0300

    Removed files from sonar

--- sonar-config.js
+++ sonar-config.js
@@ -6,9 +6,9 @@ scanner(
     token: '8157412164522b64660f5fad075c4f7ae562bc9e',
     options: {
       'sonar.projectKey': 'pos-tech-soat08-03_easyOrder-challenge4-app-produto',
-      'sonar.organization' : 'pos-tech-soat08-03',
+      'sonar.organization': 'pos-tech-soat08-03',
       'sonar.sources': './src',
-      'sonar.exclusions': '**/tests/**',
+      'sonar.exclusions': '**/tests/**, **/node_modules/**, src/swagger.ts, src/app.ts',
       'sonar.tests': './src/tests',
       'sonar.test.inclusions': './src/tests/**/*.test.tsx,./src/tests/**/*.test.ts',
       'sonar.typescript.lcov.reportPaths': 'coverage/lcov.info',
